a48bc4dd79c553becdfcce7a2e1e5c1b8a6abe53
bilal-qadir123 <bilalqadir2020@gmail.com>
2026-08-08 19:36:12 +0500

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isValid(String s) {
+        if (s.length() == 1) {
+            return false;
+        }
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
+                stack.push(s.charAt(i));
+                continue;
+            }
+            if (s.charAt(i) == ')' && stack.pop() != '(') {
+                return false;
+            }
+            if (s.charAt(i) == '}' && stack.pop() != '{') {
+                return false;
+            }
+            if (s.charAt(i) == ']' && stack.pop() != '[') {
+                return false;
+            }
+        }
+        if (!stack.isEmpty()) {
+                return false;
+        }
+        return true;
+    }
+}
