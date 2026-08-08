35d90035754ed9a87bbe7adafcc2d3550d75811f
bilal-qadir123 <bilalqadir2020@gmail.com>
2026-08-08 16:52:17 +0500

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isValid(String s) {
+        int low = 0;
+        int high = s.length() - 1;
+
+        while (low < high) {
+            if (s.charAt(low) == s.charAt(high)) {
+                return false;
+            }
+            low ++;
+            high --;
+        }
+        return true;
+    }
+}
