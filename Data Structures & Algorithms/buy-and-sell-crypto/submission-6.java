187449e44d31e29c558ef480cde7b6db8d3f5518
bilal-qadir123 <bilalqadir2020@gmail.com>
2026-08-25 18:03:02 +0500

    Add: buy-and-sell-crypto - submission-6

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-6.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int left = 0;
+
+        for (int right = 1; right < prices.length; right++) {
+            int profit = prices[right] - prices[left];
+
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
+            if (prices[right] < prices[left] && left < prices.length - 1) {
+                left = right;
+            }
+        }
+        return maxProfit;
+    }
+}
